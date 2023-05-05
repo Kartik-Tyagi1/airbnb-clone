515188f84d46b52d675fe63ba0bc0741a8d36e9d
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-05-04 22:35:35 -0400

    Created action to get reservations and block out those dates on calendar for listing

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,54 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: IParams) {
+  try {
+    const { listingId, userId, authorId } = params;
+    const query: any = {};
+
+    // Get all reservations for a specific listing
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    // Get all the trips a user has
+    if (userId) {
+      query.userId = userId;
+    }
+
+    // Get all reservations made for a user's own property
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
