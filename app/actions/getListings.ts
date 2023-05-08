8ca6f1ff6b59eff8376fedc4dc72ea145b078791
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2023-05-08 15:15:53 -0400

    Added filters for get listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,80 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  locationValue?: string;
+  startDate?: string;
+  endDate?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    // The '+' converts the string param into a number
+    // The 'gte' is 'greater than or equal' so we get listins with greater than or equal the inputted room count
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    // Filter out reservations that are already made in that range
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    if (category) {
+      query.category = category;
     }
 
     const listings = await prisma.listing.findMany({
